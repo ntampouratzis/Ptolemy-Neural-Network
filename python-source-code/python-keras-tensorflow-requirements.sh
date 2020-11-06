47b8b4414a6ef9903c65dcc3062f87721e27bd24
Panagiotis <pmousoul@gmail.com>
2020-11-07 00:43:20 +0200

    Added the python source code.

---
+++ python-source-code/python-keras-tensorflow-requirements.sh
@@ -0,0 +1,3 @@
+sudo apt-get install python3-tk
+sudo -H python3 -m pip install --upgrade pip
+python3 -m pip install matplotlib wheel pandas scikit-learn scipy keras==2.3.1 tensorflow-cpu==2.2.0 setuptools==41.0.0 tensorflow==2.2.0
